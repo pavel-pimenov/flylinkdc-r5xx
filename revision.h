35c2659c2610c340dec64b8f9ae09489071bccca
pavel.pimenov <pavel.pimenov@gmail.com>
2013-10-06 13:59:32 +0400

    # Добавил revision.h (пока не научился генерировать его из гит-а # .gitignore

---
+++ revision.h
@@ -0,0 +1,9 @@
+#ifndef FLY_REVISION_H 
+#define FLY_REVISION_H 
+ 
+#define VERSION_NUM 502 
+#define REVISION_NUM 15652 
+#define BETA 
+#define BETA_NUM   2 // Number of beta. Does not matter if the #define BETA is disabled. 
+ 
+#endif 
